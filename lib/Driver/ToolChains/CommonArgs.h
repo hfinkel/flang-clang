6ebb2983b15e0dab18794a8d78a6659d5af693d9
Hal Finkel <hfinkel@anl.gov>
2017-05-25 02:48:19 +0000

    Add llvm::opt:: to ArgList

--- lib/Driver/ToolChains/CommonArgs.h
+++ lib/Driver/ToolChains/CommonArgs.h
@@ -20,9 +20,9 @@ namespace clang {
 namespace driver {
 namespace tools {
 
-bool needFortranLibs(const Driver &D, const ArgList &Args);
+bool needFortranLibs(const Driver &D, const llvm::opt::ArgList ArgList &Args);
 
-bool needFortranMain(const Driver &D, const ArgList &Args);
+bool needFortranMain(const Driver &D, const llvm::opt::ArgList ArgList &Args);
 
 void addPathIfExists(const Driver &D, const Twine &Path,
                      ToolChain::path_list &Paths);
